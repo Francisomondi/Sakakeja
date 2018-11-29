5fc7e2a6af46d220ba1f2b7693b59b0e92293063
francisomondi <francismcjohn@gmail.com>
2018-11-29 15:51:20 +0300

    added apartments to the layout

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -11,13 +11,13 @@
                         @if(count($apartments)>0)
 
                         @foreach($apartments as $apartment)
-                      <div class="col-md-3 selector-for-some-widget ">
-                        <div class="card mb-3 shadow-sm" >
+                      <div class="col-md-2 selector-for-some-widget ">
+                        <div class="card mb-2 shadow-sm" >
                         <h3>{{$apartment->name}}</h3>
                         <img class="card-img-top" src="/storage/cover_images/{{ $apartment->cover_image }}" alt="apartment cover image">
                           <div class="card-body">
                             <p class="card-text">{{$apartment->description}}.</p>
-                            <h3>{{$apartment->category}}</h3>
+                            <button class="btn btn-default " style="aligh: center">{{$apartment->category}}</button>
                             <div class="d-flex justify-content-between align-items-center">
                               <div class="btn-group">
                                 <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
